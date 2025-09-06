a8c240198cb913b503a13dfcd23c63157a25cdc0
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2025-09-06 22:29:19 +0530

    add simple store timing logic

--- src/App.jsx
+++ src/App.jsx
@@ -72,6 +72,11 @@ function Menu() {
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 8;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
   return (
     <footer>{new Date().toLocaleTimeString()}. We're currently open.</footer>
   );
