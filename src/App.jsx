c1058e837ebfbd042bf90163370afb36bac8772f
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2025-09-08 19:15:20 +0530

    conditional using ternary operator

--- src/App.jsx
+++ src/App.jsx
@@ -70,7 +70,7 @@ function Menu() {
     <main className={"menu"}>
       <h2>Our Menu</h2>
 
-      {numPizzas && (
+      {numPizzas > 0 ? (
         <ul className={"pizzas"}>
           {pizzas.map(pizza => (
             <Pizza
@@ -79,6 +79,8 @@ function Menu() {
             />
           ))}
         </ul>
+      ) : (
+        <p>We're still working on our menu. Please come back later :)</p>
       )}
     </main>
   );
@@ -92,11 +94,15 @@ function Footer() {
 
   return (
     <footer className={"footer"}>
-      {isOpen && (
+      {isOpen ? (
         <div className='order'>
           <p>We're open until {closeHour}:00. Come visit us or order online.</p>
           <button className={"btn"}>ORDER</button>
         </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00
+        </p>
       )}
     </footer>
   );
