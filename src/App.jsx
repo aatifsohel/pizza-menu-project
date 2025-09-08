9ac0e47f47d9774002ef822340e973e9cc760284
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2025-09-08 19:36:36 +0530

    add title & conditional styling CSS

--- src/App.jsx
+++ src/App.jsx
@@ -115,10 +115,10 @@ function Footer() {
 }
 
 function Pizza({ pizzaObj }) {
-  if (pizzaObj.soldOut) return null;
+  //   if (pizzaObj.soldOut) return null;
 
   return (
-    <li className={"pizza"}>
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : " "}`}>
       <img
         src={pizzaObj.photoName}
         alt={pizzaObj.name}
@@ -126,7 +126,7 @@ function Pizza({ pizzaObj }) {
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.price}</span>
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
       </div>
     </li>
   );
